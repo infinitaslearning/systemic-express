90fe8967a7faaa1d2043894ffbf113c6fe0665ba
Teun Mooij <tmooij@infinitaslearning.com>
2021-12-12 15:08:15 +0100

    prettier

--- index.d.ts
+++ index.d.ts
@@ -24,22 +24,17 @@ export type MiddleWareConfig = {
  * Systemic component that creates and configures an Express app
  * @param options optionally a function that creates a basic Express app can be passed into the component
  */
-export function app(options?: {
-  express?: () => Express
-}): Component<Express, { config?: AppConfig; logger?: any }>
+export function app(options?: { express?: () => Express }): Component<Express, { config?: AppConfig; logger?: any }>
 
 /**
  * Starts the Express server
  */
-export function server(): Component<
-  void,
-  { config: ServerConfig; app: Express; logger?: any }
->
+export function server(): Component<void, { config: ServerConfig; app: Express; logger?: any }>
 
 /**
  * Adds default notFound and error middleware to the app
  * @param options Custom notfound and error middleware can be passed in. If omitted default middleware will be used.
  */
 export function defaultMiddleware(
-  options?: MiddlewareOptions
+  options?: MiddlewareOptions,
 ): Component<void, { config?: MiddleWareConfig; app: Express; logger?: any }>
