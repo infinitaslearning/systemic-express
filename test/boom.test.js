0e6f1c052309c3000ee59279d350158a98d30cb5
Riccardo <3663496+paciops@users.noreply.github.com>
2024-07-18 16:11:01 +0200

    Boom update (#2)
    * feat: exported error middleware into an external closure
    
    * feat: added tests for error middleware and app
    
    * feat: introduced `@hapi/boom` test
    
    * feat: updated CI
    
    * chore: formatting and new error name
    
    * fix!: removed `boom` for `@hapi/boom`
    
    * chore: formatting

---
+++ test/boom.test.js
@@ -0,0 +1,28 @@
+const { describe, it, skip } = require("mocha");
+const { strictEqual, deepStrictEqual } = require("node:assert/strict");
+const newBoom = require("@hapi/boom");
+
+/**
+ * This test is here to prove that the old Boom.wrap method
+ * is equal to the new Boom class.
+ * This test exit with 0 if the boom library is not installed.
+ * It should not be installed since it is deprecated https://www.npmjs.com/package/boom
+ */
+describe("Booom testing", () => {
+	const message = "Something went wrong";
+	it("wrap", (done) => {
+		let oldBoom;
+		try {
+			oldBoom = require("boom");
+		} catch (e) {
+			console.log("\tBoom is not installed and this test will be skipped");
+			done();
+		}
+		const errorWrap = oldBoom.wrap(new Error(message));
+		const errorNew = new newBoom.Boom(new Error(message));
+
+		strictEqual(errorNew.message, errorWrap.message);
+		strictEqual(errorNew.isBoom, errorWrap.isBoom);
+		deepStrictEqual(errorNew.output, errorWrap.output);
+	});
+});
---
+++ test/errorMiddleware.test.js
@@ -0,0 +1,38 @@
+const { describe, it } = require("mocha");
+const { spy } = require("sinon");
+const { strictEqual, deepStrictEqual } = require("node:assert/strict");
+const { errorMiddleware } = require("../lib/errorMiddleware");
+
+describe("errorMiddleware", () => {
+	const error = new Error("Something went wrong");
+	const request = { accepts: spy() };
+	const response = { status: spy(), send: spy() };
+	const logger = { log: spy() };
+
+	it("Should handle errors", async () => {
+		// Act
+		errorMiddleware({ logger, config: {} })(error, request, response, () => {});
+
+		// Assert
+		const loggedError = logger.log.lastCall.args.at(0);
+		strictEqual(loggedError.message, error.message);
+		strictEqual(loggedError.name, error.name);
+
+		const lastResponseStatusArg = response.status.lastCall.args.at(0);
+		strictEqual(lastResponseStatusArg, 500);
+
+		const lastResponseSendArg = response.send.lastCall.args.at(0);
+		strictEqual(
+			lastResponseSendArg,
+			"500 An internal server error occurred\n\n",
+		);
+
+		const lastRequestAcceptsArg = request.accepts.args;
+
+		strictEqual(lastRequestAcceptsArg[0][0], "json");
+		deepStrictEqual(lastRequestAcceptsArg[1][0], [
+			"application/xml",
+			"text/xml",
+		]);
+	});
+});
