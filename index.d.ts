47beca13ecd795a0f0a3e7bda42b6a8ce02cc3f3
riccardo <riccardo960@gmail.com>
2024-07-18 17:20:12 +0200

    chore: formatted code

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,44 @@
-import { CallbackComponent } from 'systemic'
-import { ErrorRequestHandler, Express, RequestHandler } from 'express'
+import type { CallbackComponent } from "systemic";
+import type { ErrorRequestHandler, Express, RequestHandler } from "express";
 
 export type AppConfig = {
-  settings: Record<string, any>
-}
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  settings: Record<string, any>;
+};
 
 export type ServerConfig = {
-  port: number
-  host?: string
-  shutdown?: { delay?: string }
-}
+  port: number;
+  host?: string;
+  shutdown?: { delay?: string };
+};
 
 type MiddlewareOptions = {
-  notFound?: RequestHandler
-  error?: ErrorRequestHandler
-}
+  notFound?: RequestHandler;
+  error?: ErrorRequestHandler;
+};
 
 export type MiddleWareConfig = {
-  showErrorDetail?: boolean
-}
+  showErrorDetail?: boolean;
+};
 
 /**
  * Systemic component that creates and configures an Express app
  * @param options optionally a function that creates a basic Express app can be passed into the component
  */
-export function app(options?: { express?: () => Express }): CallbackComponent<Express, { config?: AppConfig; logger?: any }>
+export function app(options?: { express?: () => Express }): CallbackComponent<
+  Express,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  { config?: AppConfig; logger?: any }
+>;
 
 /**
  * Starts the Express server
  */
-export function server(): CallbackComponent<void, { config: ServerConfig; app: Express; logger?: any }>
+export function server(): CallbackComponent<
+  void,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  { config: ServerConfig; app: Express; logger?: any }
+>;
 
 /**
  * Adds default notFound and error middleware to the app
@@ -37,4 +46,5 @@ export function server(): CallbackComponent<void, { config: ServerConfig; app: E
  */
 export function defaultMiddleware(
   options?: MiddlewareOptions,
-): CallbackComponent<void, { config?: MiddleWareConfig; app: Express; logger?: any }>
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+): CallbackComponent<void, { config?: MiddleWareConfig; app: Express; logger?: any }>;
