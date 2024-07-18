6b98ebd945f476beeff19311aec65ec63c704318
riccardo <riccardo960@gmail.com>
2024-07-18 12:19:27 +0200

    feat: added tests for error middleware and app

---
+++ test/app.test.js
@@ -0,0 +1,45 @@
+const { describe, it } = require("mocha");
+const request = require("supertest");
+const express = require("express");
+
+const System = require("systemic");
+
+const { app } = require("../index");
+
+const boomUrl = "/boom";
+
+const myErrorHandler = (error, req, res, next) => {
+	const message = "Something went wrong";
+	console.error(message, error.stack);
+	res.status(500).send(message);
+};
+
+const routes = {
+	start: async ({ app }) => {
+		app.get(boomUrl, () => {
+			throw new Error("Expected error");
+		});
+		app.use(myErrorHandler);
+	},
+};
+
+describe("app", () => {
+	it("should run", (done) => {
+		const system = new System()
+			.add("app", app())
+			.add("routes", routes)
+			.dependsOn("app");
+
+		system.start((err, { app }) => {
+			if (err) throw err;
+
+			request(app)
+				.get(boomUrl)
+				.expect(500, "Something went wrong")
+				.then(() => done())
+				.catch((err) => {
+					throw err;
+				})
+		});
+	});
+});
