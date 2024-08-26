17a434595a8fe484d235a0f15c93e0efbb1a08ae
Zsolt Burzas <burzas.zsolt@gmail.com>
2024-08-26 13:31:30 +0200

    Add body parser limit option to app (#5)
    * feat: add body parser limit option to app
    
    * fix: handle undefined app options

--- index.d.ts
+++ index.d.ts
@@ -20,11 +20,16 @@ export type MiddleWareConfig = {
   showErrorDetail?: boolean
 }
 
+export type AppOptions = {
+  express?: () => Express;
+  jsonParserLimit?: string | number
+}
+
 /**
  * Systemic component that creates and configures an Express app
  * @param options optionally a function that creates a basic Express app can be passed into the component
  */
-export function app(options?: { express?: () => Express }): CallbackComponent<Express, { config?: AppConfig; logger?: any }>
+export function app(options?: AppOptions): CallbackComponent<Express, { config?: AppConfig; logger?: any }>
 
 /**
  * Starts the Express server
